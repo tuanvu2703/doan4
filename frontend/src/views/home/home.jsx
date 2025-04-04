f413fa06cb9aa9eb4282f1f0e589e486a4d761b7
TuanVux <tuanvukg2703@gmail.com>
2025-04-04 16:33:00 +0700

    fix css text-wrap

--- frontend/src/views/home/home.jsx
+++ frontend/src/views/home/home.jsx
@@ -18,7 +18,7 @@ function Home() {
         fetchdata()
     }, [])
     return (
-        <div className="flex justify-center max-w-[800px] px-4 sm:px-6 lg:px-6">
+        <div className="flex justify-center max-w-2xl px-4 sm:px-6 lg:px-6">
             <div className="grid gap-5 mt-3 rounded-md pb-4 w-full">
                 {/* <Story /> */}
                 <PostStatus user={user} />
