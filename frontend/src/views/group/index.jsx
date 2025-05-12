9e3710da29ed2ad6744b44304b8599a19bdef114
TuanVux <tuanvukg2703@gmail.com>
2025-05-13 03:05:05 +0700

    ,.

--- frontend/src/views/group/index.jsx
+++ frontend/src/views/group/index.jsx
@@ -4,27 +4,36 @@ import { useState, useEffect, useCallback } from 'react'
 import { profileUserCurrent } from '../../service/ProfilePersonal';
 import { useParams } from 'react-router-dom';
 import AllPostGroup from './AllPostGroup';
+import { getMemberGroup } from '../../service/publicGroup';
 export default function Group() {
     const [user, setUser] = useState({})
+    const { groupid } = useParams();
     const [addNewPostFunction, setAddNewPostFunction] = useState(null);
-    const [groups, setGroups] = useState({});
+    const [memberGroup, setMemberGroup] = useState([])
     useEffect(() => {
         const fetchdata = async () => {
             const response = await profileUserCurrent();
+            const responseMemberGroup = await getMemberGroup(groupid);
+            setMemberGroup(responseMemberGroup)
             if (response && response.data) {
                 setUser(response.data)
             }
         }
         fetchdata()
-    }, [])
-    const { groupid } = useParams();
+    }, [groupid])
     // Use useCallback to maintain function reference stability
     const handlePostsUpdated = useCallback((addNewPost) => {
         setAddNewPostFunction(() => addNewPost);
     }, []);
     return (
         <div>
-            <PostStatus user={user} addNewPost={addNewPostFunction} groupId={groupid} />
+            {
+                memberGroup.some(m => m.member && m.member._id === user._id) ? (
+                    <PostStatus user={user} addNewPost={addNewPostFunction} groupId={groupid} />
+                ) : (
+                    <div>Bạn không phải là thành viên của nhóm, không có quyền đăng bài viết</div>
+                )
+            }
             <AllPostGroup onPostsUpdated={handlePostsUpdated} groupId={groupid} />
         </div>
     )
