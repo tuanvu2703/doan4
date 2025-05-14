13191fdc846314d6857f732d6e7251e037cc9979
Nemo <76892046+NemoJohnFynz@users.noreply.github.com>
2025-05-14 18:05:58 +0700

    fix resset password file change: - user.service.ts - user.controller.ts - resetpassword.dto.ts

--- backend/src/user/dto/resetPassword.dto.ts
+++ backend/src/user/dto/resetPassword.dto.ts
@@ -1,8 +1,19 @@
 
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, MinLength } from 'class-validator';
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class ResetPasswordDto {
+
+  @ApiProperty({ example: 'tienyeuai2600@gmail.com', required: true })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ example: 'YourOldPass', required: true })
+  @IsString()
+  @MaxLength(6)
+  otp: string;
+  
+
   @ApiProperty({ example: 'YourNewPass', required: true })
   @IsString()
   @MinLength(6)
